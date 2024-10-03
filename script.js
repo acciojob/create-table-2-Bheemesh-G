c6503961d7effe975e877f5e50ae0fcea6f11ef0
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-03 22:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+
+  var rows = prompt("Input number of rows");
+  var columns = prompt("Input number of columns");
+  var table = document.getElementById('myTable');
+
+	for(let i=0;i<rows;i++)
+		{
+			var newRow = table.insertRow();
+			for(let j=0;j<columns;j++)
+				{
+					var newCell = newRow.insertCell(j);
+					newCell.textContent = "Row-"+i+" "+"Column-"+" "+j;
+				}
+		}
+	
   
 }
